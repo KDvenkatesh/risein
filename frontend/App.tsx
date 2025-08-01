caf638a3a65e57aa696c5db863e6f6da7cfae8e8
K DHANU VENKATESH <venkyktmkid@gmail.com>
2025-08-01 23:12:26 +0530

    Update App.tsx

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,40 +1,20 @@
-import { useWallet } from "@aptos-labs/wallet-adapter-react";
-// Internal Components
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { Header } from "@/components/Header";
-import { WalletDetails } from "@/components/WalletDetails";
-import { NetworkInfo } from "@/components/NetworkInfo";
-import { AccountInfo } from "@/components/AccountInfo";
-import { TransferAPT } from "@/components/TransferAPT";
-import { MessageBoard } from "@/components/MessageBoard";
-import { TopBanner } from "@/components/TopBanner";
+import React from "react";
+import Home from './home';
+import Fp from './Firstpage';
+import {BrowserRouter, Route , Routes} from 'react-router-dom';
 
-function App() {
-  const { connected } = useWallet();
 
-  return (
+export default function App(){
+  return(
     <>
-      <TopBanner />
-      <Header />
-      <div className="flex items-center justify-center flex-col">
-        {connected ? (
-          <Card>
-            <CardContent className="flex flex-col gap-10 pt-6">
-              <WalletDetails />
-              <NetworkInfo />
-              <AccountInfo />
-              <TransferAPT />
-              <MessageBoard />
-            </CardContent>
-          </Card>
-        ) : (
-          <CardHeader>
-            <CardTitle>To get started Connect a wallet</CardTitle>
-          </CardHeader>
-        )}
-      </div>
+    <BrowserRouter>
+    <Routes>
+      {/* <Home/> */}
+      
+      <Route path="/" element={<Home/>}/>
+      <Route path="/first" element={<Fp/>} />
+    </Routes>
+    </BrowserRouter>
     </>
   );
 }
-
-export default App;
